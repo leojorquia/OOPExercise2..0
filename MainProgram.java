121d3ad5b92b893c19145e0c6ea04baee7d0ec81
leojorquia <116174542+leojorquia@users.noreply.github.com>
2022-10-29 10:09:23 +0800

    Add files via upload

---
+++ MainProgram.java
@@ -0,0 +1,20 @@
+public class MainProgram {
+	public static void main(String[] args) {
+		Account acc;
+		subaccount_1 acc_1= new subaccount_1("Leo Jorquia","21100447","jo1234");
+		subaccount_2 acc_2= new subaccount_2("Tyrone Dagalea","20120022","ty233");
+		acc = acc_1;
+		System.out.println(acc.ForgotPass());
+		System.out.println("This is your first account.");
+		System.out.println("Accoount name: "+acc.getName());
+		System.out.println("Account number: "+acc.getId());
+		System.out.println("Password: "+acc_1.getPassword());
+		
+		acc = acc_2;
+		System.out.println("\nThis is your second account.");
+		System.out.println("Accoount name: "+acc.getName());
+		System.out.println("Account number: "+acc.getId());
+		System.out.println("Password: "+acc_2.getPassword());
+		
+	}
+}
